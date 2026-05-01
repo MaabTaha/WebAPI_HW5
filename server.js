ef07ca472322fc847d4b63e31a99213f970a577f
MaabTaha <mgtaha22@gmail.com>
2026-05-01 11:01:33 -0600

    Assignment completed

--- server.js
+++ server.js
@@ -360,6 +360,9 @@ router.get('/movies/:id', authJwtController.isAuthenticated, async (req, res) =>
   }
 });
 
+
+
+
 app.use('/', router);
 
 const PORT = process.env.PORT || 8080; // Define PORT before using it
